be3d92837d64c5416ed7ab8bc95ba94f00d582cf
Anderson David <Anderson@pal-nat186-0-154.itap.purdue.edu>
2018-09-15 22:52:03 -0400

    Fixed the take photo

--- app/src/main/java/com/example/anderson/helloworldhackathon/TakePhoto.java
+++ app/src/main/java/com/example/anderson/helloworldhackathon/TakePhoto.java
@@ -1,12 +1,27 @@
 package com.example.anderson.helloworldhackathon;
 
 import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.anderson.helloworldhackathon.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+
 public class TakePhoto extends AppCompatActivity {
+
+    File picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +30,42 @@ public class TakePhoto extends AppCompatActivity {
 
         Intent in = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(in,0);
-
     }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File picture = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+        }
+    };
+
+    private static File getOutputMediaFile(int type){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+
 }
+
